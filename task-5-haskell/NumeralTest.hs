94d53395a8ab729e06e75dc4a4e9b036b0174963
hexakon <hex4nova@gmail.com>
2024-09-27 20:58:53 +0200

    NumeralTest done

--- task-5-haskell/NumeralTest.hs
+++ task-5-haskell/NumeralTest.hs
@@ -3,11 +3,21 @@ import Test.HUnit
 romanNumeralToInteger :: String -> Integer
 romanNumeralToInteger [] = 0
 romanNumeralToInteger (x:y:xs)
-    | x == 'M' = 1000 + romanNumeralToInteger (y:xs)
-    | x == 'D' = 500 + romanNumeralToInteger (y:xs)
-    | x == 'C' = 100 + romanNumeralToInteger (xs)
--- inte klar lol, fixar efter övningen
-
+    | [x,y] == "CM" = 900 + romanNumeralToInteger xs
+    | [x,y] == "CD" = 400 + romanNumeralToInteger xs
+    | [x,y] == "XC" = 90 + romanNumeralToInteger xs
+    | [x,y] == "XL" = 40 + romanNumeralToInteger xs
+    | [x,y] == "IX" = 9 + romanNumeralToInteger xs
+    | [x,y] == "IV" = 4 + romanNumeralToInteger xs
+romanNumeralToInteger (x:xs)
+    | x == 'M' = 1000 + romanNumeralToInteger xs
+    | x == 'D' = 500 + romanNumeralToInteger xs
+    | x == 'C' = 100 + romanNumeralToInteger xs
+    | x == 'L' = 50 + romanNumeralToInteger xs
+    | x == 'X' = 10 + romanNumeralToInteger xs
+    | x == 'V' = 5 + romanNumeralToInteger xs
+    | x == 'I' = 1 + romanNumeralToInteger xs
+    | otherwise = romanNumeralToInteger xs
 
 test1 = 
 test2 = TestCase (assertEqual "for (romanNumeralToInteger \"XIV\")," 14 (romanNumeralToInteger "XIV"))
